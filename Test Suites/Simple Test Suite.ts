b472183d98ffdf7c00c98c9481d7e420b673c01a
thinh.luu@katalon.com <D0t4katalon>
2022-01-04 16:05:03 +0700

    Replace report

--- Test Suites/Simple Test Suite.ts
+++ Test Suites/Simple Test Suite.ts
@@ -9,11 +9,18 @@
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>90448498-6c45-4828-b338-56a95c28e995</testSuiteGuid>
    <testCaseLink>
       <guid>03f94c02-41ec-4634-93eb-6e78106f67b9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Form</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e0689a4-2ee0-4028-b3b1-9f76806516f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Form - Copy</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
