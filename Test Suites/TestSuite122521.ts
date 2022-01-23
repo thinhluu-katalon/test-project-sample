d68eb9c435f8464075334deb2321843c17c4ab29
Nguyen Van Minh <minh.nguyen@katalon.com>
2022-01-23 11:35:12 +0700

    check

---
+++ Test Suites/TestSuite122521.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite122521</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a167ed7-3d57-4dda-a873-3cf3d1984c0f</testSuiteGuid>
+   <testCaseLink>
+      <guid>1a83c29e-9ada-4c95-a267-d27758c34409</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.122521a/Folder1.Folder2a/Form121721ba</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ed7a9fbd-3fe0-472f-b365-1472abe7e2ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.122521a/Folder3.Folder4a/Form122521aa</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
