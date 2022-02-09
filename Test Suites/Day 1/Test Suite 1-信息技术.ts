84d926b9bc460c5bc853fb38e3926b9c8ea531e1
Minh Nguyen Van <minh.nguyen@katalon.com>
2022-02-09 15:30:46 +0700

    first commit

---
+++ Test Suites/Day 1/Test Suite 1-信息技术.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1-信息技术</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e1608d95-3c2c-456e-9bc4-69c90750e82f</testSuiteGuid>
+</TestSuiteEntity>
