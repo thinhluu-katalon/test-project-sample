12f26a8d3a1edd47e6cf2d49ba9d2892a2b372c1
Minh Nguyen Van <minh.nguyen@katalon.com>
2022-02-10 14:52:45 +0700

    first commit

--- Test Suites/Test.Suite.1/Test.Suite.2/Test.Suite.3/Test.Suite.4/Test.Suite.5/Test.Suite.6/Test.Suite.7/NewTestSuite1.ts
+++ Test Suites/Test.Suite.1/Test.Suite.2/Test.Suite.3/Test.Suite.4/Test.Suite.5/Test.Suite.6/Test.Suite.7/NewTestSuite1.ts
@@ -12,9 +12,15 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>3caf74a1-d14a-4157-9cac-8cafa2b6e43f</testSuiteGuid>
    <testCaseLink>
-      <guid>274e1ef0-4511-4b66-9822-349e7316ca4a</guid>
+      <guid>e35b257e-3673-4d23-a274-0e79f8c56d1e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Common Test Case/Login</testCaseId>
+      <testCaseId>Test Cases/Test.Case.121721a/Folder3.Folder4a/Form121721aa_new</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>546820ac-6998-478a-ae78-1f7a922666a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.122521a/Folder1.Folder2a/Form121721ba</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/TestSuiteCollection121721.ts
+++ Test Suites/TestSuiteCollection121721.ts
@@ -15,14 +15,5 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Test.Suite.1/Test.Suite.2/Test.Suite.3/Test.Suite.4/Test.Suite.5/Test.Suite.6/Test.Suite.7/Test.Suite.8/TestSuite121721</testSuiteEntity>
       </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Test.Suite.1/Test.Suite.2/Test.Suite.3/Test.Suite.4/Test.Suite.5/Test.Suite.6/Test.Suite.7/Test.Suite.8/TestSuite121721</testSuiteEntity>
-      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
